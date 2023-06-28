47cc9a4891a5ab14172ad6c32d7b5d2aea9bb167
SergioGonzalezB11 <sagonzalezbarajas@gmail.com>
2023-06-28 09:57:40 -0500

    departamentos

--- view/Regions/mainregion.php
+++ view/Regions/mainregion.php
@@ -1,4 +1,4 @@
-<h1>Gestion de Regiones</h1>
+<h1>Gestion de Departamentos</h1>
 <ul class="nav nav-tabs">
     <li class="nav-item">
         <a class="nav-link active" aria-current="page" href="#">Active</a>
